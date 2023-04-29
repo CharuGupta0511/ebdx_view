8a751672695c6852006c9de7a17f79882c34d71d
CharuGupta0511 <charurawatt@gmail.com>
2023-04-30 09:16:17 +1000

    adding to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Row,Col, Container, Card} from "reactstrap";
+import { BrowserRouter,Route,Routes,NavLink } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Language from './Components/Language';
+import logo from './image/logo.jpg';
+import { ToastContainer } from 'react-toastify';
+import SelectionMenu from './Components/SelectionMenu';
+import AllList from './Components/AllList';
+import OneList from './Components/OneList';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <BrowserRouter>
+            <div style={{
+                display: "flex",
+                background: 'lightblue',
+                padding: '5px 0 5px 5px',
+                fontSize: '20px',
+            }}>
+                <div style={{ margin: '10px' }}>
+                    <NavLink to="/" style={({ isActive }) => ({ 
+                        color: isActive ? 'green' : 'white' })}>
+                        Home
+                    </NavLink>
+                </div>
+                <div style={{ margin: '10px' }}>
+                    <NavLink to="/Language" style={({ isActive }) => ({ 
+                        color: isActive ? 'green' : 'white' })}>
+                        Language
+                    </NavLink>
+                </div>
+                <div style={{ margin: '10px' }}>
+                    <NavLink to="/About" style={({ isActive }) => ({ 
+                        color: isActive ? 'green' : 'white' })}>
+                        About
+                    </NavLink>
+                </div>
+            </div>
+            <div>
+            <ToastContainer />
+            <Container>
+                <Row>
+                    <Col md={4}>
+                        <React.Fragment>
+                            <SelectionMenu />
+                        </React.Fragment>    
+                    </Col>
+                    <Col md={8}>
+                        <div style={{height: '150px', width: '950px', margin: '5px'}}>
+                            <label style={{color: 'green'}}>Interchange Number</label>{' '}
+                        <input name="SearchTextbox" type="textbox"/> {' '}
+                                <button style={{background: 'green', color: 'white'}}>Go</button>
+                        <div className='logo' style={{float : 'right', margin : '10px'}}>
+                <img src={logo} height={100} width={200} /> 
+            </div>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/Language" element={<Language />} />
+                <Route exact path="/About" element={<About />} />
+            </Routes>
+            </div>
+            <AllList />
+        </Col>
+        </Row>
+        </Container>
+            </div>
+        </BrowserRouter>
+        
+    </>
+);
 }
 
 export default App;
